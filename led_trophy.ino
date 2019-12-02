e4cb665ee2361c8af32790d6a9b88cb31da6aaed
Matt Davidson <mdavidson@allureglobal.com>
2019-12-02 12:31:40 -0500

    cleaning up

--- led_trophy.ino
+++ led_trophy.ino
@@ -4,8 +4,8 @@
 #define CYCLE_DELAY 50
 #define LOOP_DELAY 0
 
-int rows = 5;
-int columns = 8;
+#define ROWS 5
+#define COLUMNS 8
 
 #define OUTER_GAMMA_MIN 0
 #define OUTER_GAMMA_MAX 65536
@@ -13,29 +13,28 @@ uint16_t outer_gamma = 0;
 
 #define INNER_GAMMA_MIN 0
 #define INNER_GAMMA_MAX 255
-int inner_gamma = 255;
+#define INNER_HUE 0
+#define INNER_SATURATION 0
+#define INNER_GAMMA 255
+//int inner_gamma = 255;
 int inner_active = 0;
 int inner_rows[] = {1,2,3};
 
-Adafruit_NeoPixel strip(rows * columns, LED_PIN, NEO_GRB+NEO_KHZ800);
+Adafruit_NeoPixel strip(ROWS * COLUMNS, LED_PIN, NEO_GRB+NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
   strip.begin();
   strip.show();
-  strip.setBrightness(25);
+  strip.setBrightness(10);
 }
 
 void loop() {
   strip.clear();
-  strip.fill(strip.ColorHSV(outer_gamma),0,8);
-  strip.fill(strip.ColorHSV(outer_gamma),32,8);
-  strip.fill(strip.Color(inner_gamma, inner_gamma, inner_gamma), inner_rows[inner_active]*columns, columns);
+  strip.fill(strip.ColorHSV(outer_gamma),0,COLUMNS);
+  strip.fill(strip.ColorHSV(outer_gamma),32,COLUMNS);
+  strip.fill(strip.ColorHSV(INNER_HUE, INNER_SATURATION, INNER_GAMMA), inner_rows[inner_active]*COLUMNS, COLUMNS);
   strip.show();
   if (0 == outer_gamma%CYCLE_DELAY) {
-    Serial.print("Cycling row ");
-    Serial.println(outer_gamma);
     inner_active++;
   }
   if (inner_active >= (sizeof(inner_rows)/sizeof(inner_rows[0]))) {
